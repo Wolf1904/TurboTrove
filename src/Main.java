72b189cde8a0fcb51a5d07892438cd18bc9ece18
Wolf1904 <wolf190403@gmail.com>
2024-08-03 01:16:10 +0530

    "Extracted switch statement logic into separate methods for handling different menu choices."

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ import java.util.logging.Level;
 public class Main {
     private static final Logger logger = Logger.getLogger(Main.class.getName());
 
-    
     static void mainMenu() {
         logger.info("");
         logger.info("======================= *** WELCOME TO TURBOTROVE *** =======================");
@@ -39,73 +38,96 @@ public class Main {
             choice = sc.nextInt();
             
             while (choice != 9 && choice != 0) {
-                switch (choice) {
-                    case 1:
-                        showroom[showroomCounter] = new Showroom();
-                        showroom[showroomCounter].set_details();
-                        showroomCounter++;
-                        logger.info("");
-                        logger.info("[1].ADD NEW SHOWROOM");
-                        logger.info("[9].GO BACK TO MAIN MENU");
-                        choice = sc.nextInt();
-                        break;
-                    case 2:
-                        employee[employeesCounter] = new Employees();
-                        employee[employeesCounter].set_details();
-                        employeesCounter++;
-                        logger.info("");
-                        logger.info("[2].ADD NEW EMPLOYEE");
-                        logger.info("[9].GO BACK TO MAIN MENU");
-                        choice = sc.nextInt();
-                        break;
-                    case 3:
-                        car[carCounter] = new Cars();
-                        car[carCounter].set_details();
-                        carCounter++;
-                        logger.info("");
-                        logger.info("[3].ADD NEW CAR");
-                        logger.info("[9].GO BACK TO MAIN MENU");
-                        choice = sc.nextInt();
-                        break;
-                    case 4:
-                        for (int i = 0; i < showroomCounter; i++) {
-                            showroom[i].get_details();
-                            logger.info("");
-                            logger.info("");
-                        }
-                        logger.info("");
-                        logger.info("[9].GO BACK TO MAIN MENU");
-                        logger.info("[0].EXIT");
-                        choice = sc.nextInt();
-                        break;
-                    case 5:
-                        for (int i = 0; i < employeesCounter; i++) {
-                            employee[i].get_details();
-                            logger.info("");
-                            logger.info("");
-                        }
-                        logger.info("");
-                        logger.info("[9].GO BACK TO MAIN MENU");
-                        logger.info("[0].EXIT");
-                        choice = sc.nextInt();
-                        break;
-                    case 6:
-                        for (int i = 0; i < carCounter; i++) {
-                            car[i].get_details();
-                            logger.info("");
-                            logger.info("");
-                        }
-                        logger.info("");
-                        logger.info("[9].GO BACK TO MAIN MENU");
-                        logger.info("[0].EXIT");
-                        choice = sc.nextInt();
-                        break;
-                    default:
-                        logger.log(Level.WARNING, "ENTER VALID CHOICE: ");
-                        break;
-                }
+                handleChoice(choice, sc, showroom, employee, car, showroomCounter, employeesCounter, carCounter);
+                choice = sc.nextInt();
             }
         }
         sc.close();
+    }
+
+    private static void handleChoice(int choice, Scanner sc, Showroom[] showroom, Employees[] employee, Cars[] car, int showroomCounter, int employeesCounter, int carCounter) {
+        switch (choice) {
+            case 1:
+                addShowroom(sc, showroom, showroomCounter);
+                break;
+            case 2:
+                addEmployee(sc, employee, employeesCounter);
+                break;
+            case 3:
+                addCar(sc, car, carCounter);
+                break;
+            case 4:
+                getShowrooms(showroom, showroomCounter);
+                break;
+            case 5:
+                getEmployees(employee, employeesCounter);
+                break;
+            case 6:
+                getCars(car, carCounter);
+                break;
+            default:
+                logger.log(Level.WARNING, "ENTER VALID CHOICE: ");
+                break;
+        }
+    }
+
+    private static void addShowroom(Scanner sc, Showroom[] showroom, int showroomCounter) {
+        showroom[showroomCounter] = new Showroom();
+        showroom[showroomCounter].set_details();
+        showroomCounter++;
+        logger.info("");
+        logger.info("[1].ADD NEW SHOWROOM");
+        logger.info("[9].GO BACK TO MAIN MENU");
+    }
+
+    private static void addEmployee(Scanner sc, Employees[] employee, int employeesCounter) {
+        employee[employeesCounter] = new Employees();
+        employee[employeesCounter].set_details();
+        employeesCounter++;
+        logger.info("");
+        logger.info("[2].ADD NEW EMPLOYEE");
+        logger.info("[9].GO BACK TO MAIN MENU");
+    }
+
+    private static void addCar(Scanner sc, Cars[] car, int carCounter) {
+        car[carCounter] = new Cars();
+        car[carCounter].set_details();
+        carCounter++;
+        logger.info("");
+        logger.info("[3].ADD NEW CAR");
+        logger.info("[9].GO BACK TO MAIN MENU");
+    }
+
+    private static void getShowrooms(Showroom[] showroom, int showroomCounter) {
+        for (int i = 0; i < showroomCounter; i++) {
+            showroom[i].get_details();
+            logger.info("");
+            logger.info("");
+        }
+        logger.info("");
+        logger.info("[9].GO BACK TO MAIN MENU");
+        logger.info("[0].EXIT");
+    }
+
+    private static void getEmployees(Employees[] employee, int employeesCounter) {
+        for (int i = 0; i < employeesCounter; i++) {
+            employee[i].get_details();
+            logger.info("");
+            logger.info("");
+        }
+        logger.info("");
+        logger.info("[9].GO BACK TO MAIN MENU");
+        logger.info("[0].EXIT");
+    }
+
+    private static void getCars(Cars[] car, int carCounter) {
+        for (int i = 0; i < carCounter; i++) {
+            car[i].get_details();
+            logger.info("");
+            logger.info("");
+        }
+        logger.info("");
+        logger.info("[9].GO BACK TO MAIN MENU");
+        logger.info("[0].EXIT");
     }
 }
