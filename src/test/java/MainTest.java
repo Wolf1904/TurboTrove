e3751d75f7b90149d3e05851d1dcbcc6b28e3328
Wolf1904 <wolf190403@gmail.com>
2024-08-05 01:53:22 +0530

    "Removed Cars, Employees, Main, Showroom, and Utility classes; added test runner to GitHub workflow"

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MainTest {
+    @Test
+    public void testMainMenu() {
+        Main.mainMenu();
+        assertTrue(true);  // Since mainMenu doesn't return a value, this is a placeholder assertion
+    }
+}
